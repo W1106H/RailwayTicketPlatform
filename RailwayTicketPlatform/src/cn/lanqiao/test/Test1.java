c5a947a8b9d5293c9c20931472b6586a8acc18c5
Wang <2795200642@qq.com>
2021-04-11 10:12:35 +0800

    (Test里输出第二句话)

--- RailwayTicketPlatform/src/cn/lanqiao/test/Test1.java
+++ RailwayTicketPlatform/src/cn/lanqiao/test/Test1.java
@@ -2,7 +2,7 @@ package cn.lanqiao.test;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("just a practice");
+        System.out.println("just a   practice");
         System.out.println("second day");
     }
 }
