b3e61449799ad475e53fe6414682f7f6b8e92e2d
W1106H <76729368+W1106H@users.noreply.github.com>
2021-04-11 10:45:34 +0800

    Delete Test1.java

--- RailwayTicketPlatform/src/cn/lanqiao/test/Test1.java
+++
@@ -1,9 +0,0 @@
-package cn.lanqiao.test;
-
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("just a practice");
-        System.out.println("second day");
-    }
-}
-
