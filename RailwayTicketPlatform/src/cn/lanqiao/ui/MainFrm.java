d4ef580a6ba6ee5d0b95d5dd700df6d109fee745
Wang <2795200642@qq.com>
2021-04-13 18:25:15 +0800

    修改MainFrm

--- RailwayTicketPlatform/src/cn/lanqiao/ui/MainFrm.java
+++ RailwayTicketPlatform/src/cn/lanqiao/ui/MainFrm.java
@@ -8,7 +8,6 @@ import com.eltima.components.ui.DatePicker;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Date;
 import javax.swing.*;
 /**
  * @author Brainrain
@@ -20,6 +19,7 @@ public class MainFrm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 关闭窗口后操作为退出程序
         init(contentPane);
         initComponents();
+
     }
 
     /*public MainFrm(User user){
@@ -32,10 +32,10 @@ public class MainFrm extends JFrame {
         //======== this ========
         setTitle("\u94c1\u8def\u7968\u52a1\u7ba1\u7406\u5e73\u53f0");
         setFont(new Font("\u4eff\u5b8b", Font.PLAIN, 18));
-        setIconImage(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/MainLogo.png")).getImage());
-
+        setIconImage(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/mainLogo.png")).getImage());
+        contentPane.setBackground(new Color(102, 153, 255));
+        contentPane = getContentPane();
         contentPane.setLayout(null);
-        contentPane.setBackground(new Color(102, 153, 255, 110));
 
         {
             // compute preferred size
@@ -96,16 +96,16 @@ public class MainFrm extends JFrame {
         menu4 = new JMenu();
         origin = new JTextField();
         destination = new JTextField();
-        label3 = new JLabel();
         radioButton1 = new JRadioButton();
         radioButton2 = new JRadioButton();
         datePicker = new DatePicker();
         ticketSearch = new JButton();
+        label3 = new JLabel();
 
         //======== this ========
         setTitle("\u94c1\u8def\u7968\u52a1\u7ba1\u7406\u5e73\u53f0");
         setFont(new Font("\u4eff\u5b8b", Font.PLAIN, 18));
-        setIconImage(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/MainLogo.png")).getImage());
+        setIconImage(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/mainLogo.png")).getImage());
         setBackground(new Color(102, 153, 255));
         var contentPane = getContentPane();
         contentPane.setLayout(null);
@@ -175,17 +175,6 @@ public class MainFrm extends JFrame {
         contentPane.add(destination);
         destination.setBounds(520, 110, 130, 70);
 
-        //---- label3 ----
-        label3.setIcon(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/jiantou .png")));
-        label3.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                jiantouMouseClicked(e);
-            }
-        });
-        contentPane.add(label3);
-        label3.setBounds(405, 110, 65, 65);
-
         //---- radioButton1 ----
         radioButton1.setText("\u9ad8\u94c1/\u52a8\u8f66");
         radioButton1.setForeground(Color.white);
@@ -222,6 +211,17 @@ public class MainFrm extends JFrame {
         contentPane.add(ticketSearch);
         ticketSearch.setBounds(new Rectangle(new Point(365, 290), ticketSearch.getPreferredSize()));
 
+        //---- label3 ----
+        label3.setIcon(new ImageIcon(getClass().getResource("/cn/lanqiao/util/Pictures/jiantou.png")));
+        label3.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                jiantouMouseClicked(e);
+            }
+        });
+        contentPane.add(label3);
+        label3.setBounds(400, 115, 65, 65);
+
         {
             // compute preferred size
             Dimension preferredSize = new Dimension();
@@ -250,11 +250,11 @@ public class MainFrm extends JFrame {
     private JMenu menu4;
     private JTextField origin;
     private JTextField destination;
-    private JLabel label3;
     private JRadioButton radioButton1;
     private JRadioButton radioButton2;
     private DatePicker datePicker;
     private JButton ticketSearch;
+    private JLabel label3;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 
     public static void main(String[] args) {
